9d2f6f8146340859d2a3ca8eda74b8ee9d66cc84
anyel-ec <paulangel826@gmail.com>
2024-06-30 12:01:36 -0500

    start project in spring boot v3.3.1 SDK 17

---
+++ src/test/java/top/anyel/orm/SpringBootOrmRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.anyel.orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOrmRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
