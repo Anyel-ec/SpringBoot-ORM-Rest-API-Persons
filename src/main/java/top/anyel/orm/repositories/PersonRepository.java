5da9d46af7ee3ac2294f093ba7df0adea3f68c84
anyel-ec <paulangel826@gmail.com>
2024-07-01 01:16:40 -0500

    ORM to postgresql

--- src/main/java/top/anyel/orm/repositories/PersonRepository.java
+++ src/main/java/top/anyel/orm/repositories/PersonRepository.java
@@ -1,10 +1,9 @@
 package top.anyel.orm.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import top.anyel.orm.models.Person;
-
-import java.util.List;
+        import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.stereotype.Repository;
+        import top.anyel.orm.models.Person;
+        import java.util.List;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
